853440fd6f3c0c0881d673918fc9a3c0442abd35
Sergey Dreger <sergey.dreger@gmail.com>
2021-02-28 15:18:18 +0200

    Add the JIB section to the build.gradle file

--- src/main/java/ua/hazelcast/cluster/deployment/exception/ExceptionConstants.java
+++ src/main/java/ua/hazelcast/cluster/deployment/exception/ExceptionConstants.java
@@ -5,7 +5,7 @@ import java.net.URI;
 public final class ExceptionConstants {
 
     private static final String PROBLEM_BASE_URL = "https://hazelcast.com/problem";
-    
+
     public static final URI DEFAULT_TYPE = URI.create(PROBLEM_BASE_URL + "/generic-problem");
 
     private ExceptionConstants() { }
